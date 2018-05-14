adab4b2d350ee75d571e69e16579f3738e69fe69
Peter Gyory <ptgyory@gmail.com>
2018-05-14 15:10:35 -0600

    Add line prefab and organize files

---
+++ HapticVRWizard/Assets/Scripts/LineDraw.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour {
+	private LineRenderer line;
+	public List<Vector3> pointsList;
+
+	// Use this for initialization
+	void Start() {
+		line = gameObject.AddComponent<LineRenderer>();
+		line.material = new Material(Shader.Find("Particles/Additive"));
+		line.positionCount = 0;
+		line.startWidth = 0.01f;
+		line.endWidth = 0.01f;
+		line.startColor = Color.green;
+		line.endColor = Color.green;
+		line.useWorldSpace = true;
+		pointsList = new List<Vector3>();
+	}
+
+	public void addPoint(Vector3 point) {
+		pointsList.Add(point);
+		line.positionCount = pointsList.Count;
+		line.SetPosition(pointsList.Count - 1, (Vector3)pointsList[pointsList.Count - 1]);
+	}
+
+	// public void setPointsList(newList) {
+	// 	line.SetVertexCount(0);
+  //   pointsList.RemoveRange(0, pointsList.Count);
+
+	// 	pointsList = new List<Vector3>(newList);
+
+	// 	line.SetVertexCount(pointsList.Count);
+	// 	for(int i = 0; i < pointsList.Count; i++) {
+	// 		line.SetPosition(i, (Vector3)pointsList[i]);
+	// 	}
+	// }
+}
---
+++ HapticVRWizard/Assets/Scripts/LineManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour {
+	public GameObject line;
+	public List<GameObject> allLines;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void StartEntity() {
+
+	}
+
+	public void UpdateEntity() {
+		
+	}
+}
