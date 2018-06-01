ab9d69bec5b51028f90c7f8703ccd300cc908a9f
Peter Gyory <ptgyory@gmail.com>
2018-06-01 11:11:45 -0600

    Add Left Controller touchpad reading

---
+++ HapticVRWizard/Assets/ViveMenuReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveMenuReader : MonoBehaviour {
+	private bool _isPadHeld = false;
+	private SteamVR_TrackedObject _trackedObj;
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)_trackedObj.index); }
+    }
+
+	// Use this for initialization
+	void Awake () {
+		_trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+			Vector2 axis = Controller.GetAxis();
+
+			if (axis.x > 0.2f) {
+				Debug.Log("Redo!");
+			} else if (axis.x < -0.2f) {
+				Debug.Log("Undo!");
+			}
+		}
+	}
+}
