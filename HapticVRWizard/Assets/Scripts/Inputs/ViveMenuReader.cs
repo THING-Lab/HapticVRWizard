de08faa78faa6064f012acbc5f2b8051fd4fbcb4
Peter Gyory <ptgyory@gmail.com>
2018-06-06 11:01:01 -0600

    Add Rotateable Menu Element to Left Controller

--- HapticVRWizard/Assets/Scripts/Inputs/ViveMenuReader.cs
+++ HapticVRWizard/Assets/Scripts/Inputs/ViveMenuReader.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ViveMenuReader : MonoBehaviour {
 	private bool _isPadHeld = false;
+	private bool _isTouchHeld = false;
+	private float _menuRotation = 0f;
+	private Vector2 _prevTouch = new Vector2(0, 0);
 	private SteamVR_TrackedObject _trackedObj;
     private SteamVR_Controller.Device Controller {
         get { return SteamVR_Controller.Input((int)_trackedObj.index); }
     }
 
 	public ToolManager _toolManager;
+	public Transform _menuTransform;
 
 	// Use this for initialization
 	void Awake () {
@@ -27,5 +31,25 @@ public class ViveMenuReader : MonoBehaviour {
 				_toolManager.Undo();
 			}
 		}
+
+		// Menu Rotation
+        bool currentTouch = Controller.GetTouch(SteamVR_Controller.ButtonMask.Touchpad);
+        if (currentTouch && _isTouchHeld) {
+            Vector2 axis = Controller.GetAxis();
+            float dx = axis.x - _prevTouch.x;
+			// Scale dx so rotation is more comfortable
+			_menuRotation -= dx * 30;
+            
+            // Use threshold
+            if (dx > 0.0001 || dx < -0.0001) {
+                _menuTransform.localRotation = Quaternion.AngleAxis(_menuRotation, Vector3.forward);
+            }
+
+            _prevTouch = axis;
+        } else if (currentTouch) {
+            _prevTouch = Controller.GetAxis();
+        }
+
+        _isTouchHeld = currentTouch; 
 	}
 }
