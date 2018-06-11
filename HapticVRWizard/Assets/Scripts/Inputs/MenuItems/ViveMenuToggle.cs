323715fa3855ca137b431edb526207c289ef92db
Peter Gyory <ptgyory@gmail.com>
2018-06-11 16:36:52 -0600

    Allow tracker toggling and disable buttons for disabled trackers

--- HapticVRWizard/Assets/Scripts/Inputs/MenuItems/ViveMenuToggle.cs
+++ HapticVRWizard/Assets/Scripts/Inputs/MenuItems/ViveMenuToggle.cs
@@ -9,22 +9,72 @@ public class ViveMenuToggle : MonoBehaviour {
 	public UnityEvent _deselectEvent;
 
 	public bool _isSelected = false;
+	private bool _isEnabled = true;
+	public bool IsEnabled {
+		get { return _isEnabled; }
+	}
 
 	public Color _unselectedColor;
 	public Color _disabledColor;
 	public Color _selectedColor;
+	public int _trackerId;
 
-	public void Execute() {
+	private ViveMenuRadioList ParentList {
+		get { return transform.parent.gameObject.GetComponent<ViveMenuRadioList>(); }
+	}
+
+	public void Start() {
 		if (_isSelected) {
-			// Check if there is a parent manager, if so call that?
-			_isSelected = false;
-			_deselectEvent.Invoke();
-			GetComponent<Image>().color = Color.black;
-		} else {
-			// Check if there is a parent manager, if so call it's deselect function first for the others
-			_isSelected = true;
-			_selectEvent.Invoke();
 			GetComponent<Image>().color = _selectedColor;
 		}
+	}
+
+	public void Disable() {
+		// Might want to handle for if this is the selected one here...
+		_isSelected = false;
+		_isEnabled = false;
+		GetComponent<Image>().color = _disabledColor;
+	}
+
+	public void Enable() {
+		_isEnabled = true;
+		GetComponent<Image>().color = _unselectedColor;
+	}
+
+	public void Deselect() {
+		if (_isEnabled) {
+			_isSelected = false;
+			// Actual display of state
+			GetComponent<Image>().color = _unselectedColor;
+		}
+	}
+
+	public void Select() {
+		if (_isEnabled) {
+			_isSelected = true;
+			// Actual display of state
+			GetComponent<Image>().color = _selectedColor;
+		}
+	}
+
+	public void Execute() {
+		if (_isEnabled) {
+			if (_isSelected) {
+				// If there is a parent manager don't fire the deselect event
+				if (ParentList == null) {
+					Deselect();
+					_deselectEvent.Invoke();
+				}
+			} else {
+				// Check if there is a parent manager, if so call it's deselect function first for the others
+				if (ParentList != null) {
+					ParentList.DeselectOthers(_trackerId);
+				}
+				_selectEvent.Invoke();
+				Select();
+				
+			}
+		}
+		
 	}
 }
---
+++ HapticVRWizard/Assets/Scripts/Inputs/MenuItems/ViveTrackerToggleList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveTrackerToggleList : MonoBehaviour {
+	public List<GameObject> _trackers;
+	public List<ViveMenuToggle> _toggles;
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < _trackers.Count; i++) {
+			if (!_trackers[i].active && _toggles[i].IsEnabled) {
+				_toggles[i].Disable();
+			} else if (_trackers[i].active && !_toggles[i].IsEnabled) {
+				_toggles[i].Enable();
+			}
+		}
+	}
+}
