8be3ea085b40a59b1ce44ce9909d373144d89aa9
Peter Gyory <ptgyory@gmail.com>
2018-06-18 15:28:22 -0600

    Change Material of active tracker

---
+++ HapticVRWizard/Assets/Scripts/TrackerMatManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerMatManager : MonoBehaviour {
+	public List<Renderer> _trackerModels;
+	public Material _activeMat;
+	public Material _inactiveMat;
+	// Update is called once per frame
+	public void SetActiveTrackerMat(int activeId) {
+		// Is the id of a possible tracker
+		if (activeId >= 0 && activeId < _trackerModels.Count) {
+			for (int id = 0; id < _trackerModels.Count; id++) {
+				if (id == activeId) {
+					_trackerModels[id].material = _activeMat;
+				} else {
+					_trackerModels[id].material = _inactiveMat;
+				}
+			}
+		} else {
+			Debug.Log("Tracker ID out of bounds, was this intended... Probably yes ;)");
+		}
+	} 
+}
