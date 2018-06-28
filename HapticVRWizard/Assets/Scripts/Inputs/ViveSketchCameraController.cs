f55f0bda83c472380ab1b74d9d57e3a6924bdea1
Peter Gyory <ptgyory@gmail.com>
2018-06-28 09:00:00 -0600

    Allow sketch camera position setting

---
+++ HapticVRWizard/Assets/Scripts/Inputs/ViveSketchCameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveSketchCameraController : MonoBehaviour {
+	public GameObject _sketchCamera;
+	private bool _isTriggerHeld;
+
+	private SteamVR_TrackedObject _trackedObj;
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)_trackedObj.index); }
+    }
+
+	void Awake () {
+        _trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Controller.GetHairTriggerDown()) {
+            _isTriggerHeld = true;
+			_sketchCamera.transform.position = transform.position;
+        }
+
+        if (Controller.GetHairTriggerUp()) {
+            _isTriggerHeld = false;
+        }
+
+		if (_isTriggerHeld) {
+			_sketchCamera.transform.LookAt(transform.position);
+		}
+	}
+}
